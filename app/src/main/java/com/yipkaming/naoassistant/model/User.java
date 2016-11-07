2bc7c13684a6602ca0a56e1146c030f771a706ad
Rickyip <13002066d@connect.polyu.hk>
2016-11-07 21:32:55 +0800

    - updated User class

--- app/src/main/java/com/yipkaming/naoassistant/model/User.java
+++ app/src/main/java/com/yipkaming/naoassistant/model/User.java
@@ -5,4 +5,19 @@ package com.yipkaming.naoassistant.model;
  */
 
 public class User {
+
+    private final String[] GENDERS = {"Male", "Female"};
+    private String name;
+    private String gender;
+    private int age;
+    private String city;
+    private String interests;
+
+    public User(String name, String gender, int age, String city, String interests) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.city = city;
+        this.interests = interests;
+    }
 }
