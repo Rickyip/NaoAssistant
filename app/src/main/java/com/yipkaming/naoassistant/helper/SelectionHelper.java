c446d57a85408496b407e01f65adc3ecdb09c5a8
Rickyip <13002066d@connect.polyu.hk>
2016-11-08 18:11:36 +0800

    - added comment for flow

--- app/src/main/java/com/yipkaming/naoassistant/helper/SelectionHelper.java
+++ app/src/main/java/com/yipkaming/naoassistant/helper/SelectionHelper.java
@@ -1,8 +1,19 @@
 package com.yipkaming.naoassistant.helper;
 
+import com.yipkaming.naoassistant.model.Notification;
+
 /**
  * Created by Yip on 7/11/2016.
  */
 
 public class SelectionHelper {
+
+    public static void process(Notification notification){
+        // 1. important application?
+        // 2. if no, any keywords in content/ title?
+        // 3. if 1 yes or 2 yes, pass to verbal message class
+        // 4. if both no, notification.setImportance(1), and push to nao
+        // 5. notification.save()   save to db
+        // 6. end
+    }
 }
