21bced34062f38e5f7c7fda6b8c2a268e360405f
Rickyip <13002066d@connect.polyu.hk>
2016-11-20 17:52:41 +0800

    - added code in process()

--- app/src/main/java/com/yipkaming/naoassistant/helper/SelectionHelper.java
+++ app/src/main/java/com/yipkaming/naoassistant/helper/SelectionHelper.java
@@ -1,6 +1,12 @@
 package com.yipkaming.naoassistant.helper;
 
+import android.util.Log;
+
+import com.yipkaming.naoassistant.model.Config;
+import com.yipkaming.naoassistant.model.Keyword;
+import com.yipkaming.naoassistant.model.Nao;
 import com.yipkaming.naoassistant.model.NotificationMessage;
+import com.yipkaming.naoassistant.model.VerbalReminder;
 
 /**
  * Created by Yip on 7/11/2016.
@@ -9,12 +15,22 @@ import com.yipkaming.naoassistant.model.NotificationMessage;
 public class SelectionHelper {
     // Singleton pattern
 
+    private static final String TAG = Config.getSimpleName(SelectionHelper.class);
+
     private static SelectionHelper instance;
 
     private SelectionHelper(){}
 
-    public static void process(NotificationMessage notificationMessage){
-
+    public void process(NotificationMessage notificationMessage){
+        if(Keyword.findKeyWord(notificationMessage.getPackageName())){
+            VerbalReminder verbalReminder = new VerbalReminder(notificationMessage);
+            try {
+                Log.e(TAG, "Say: "+ verbalReminder.getReminder() );
+//                Nao.getInstance().say(verbalReminder.getReminder());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         // 1. important application?
         // 2. if no, any keywords in content/ title?
         // 3. if 1 yes or 2 yes, pass to verbal message class
