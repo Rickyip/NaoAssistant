6bc2ee35378f27326dd8fd24649d138357173930
Rickyip <13002066d@connect.polyu.hk>
2017-01-05 17:40:02 +0800

    - added extra content to the reminder

--- app/src/main/java/com/yipkaming/naoassistant/model/VerbalReminder.java
+++ app/src/main/java/com/yipkaming/naoassistant/model/VerbalReminder.java
@@ -16,11 +16,15 @@ public class VerbalReminder {
     public static final String COM_DOT = "com.";
     public static final String GMAIL = "Gmail";
     public static final String GM = "gm";
+    public static final String GOOGLE_ANDROID_GM = "google.android.gm";
     public static final String HEADER = "You have a message ";
+    public static final String FROM = " from ";
     private String time;
     private String content;
-    private String app = "from ";
+    private String app = FROM;
     private String reminder;
+    private String extraMsg;
+    private String title;
 
     public VerbalReminder(String time, String content) {
         this.time = time;
@@ -30,18 +34,32 @@ public class VerbalReminder {
     public VerbalReminder(NotificationMessage notificationMessage) {
         this.time = String.valueOf(notificationMessage.getTime());
         this.content = notificationMessage.getContent();
+        this.extraMsg = notificationMessage.getExtraContent();
+        this.title = notificationMessage.getTickerText();
+
         String packageName = notificationMessage.getPackageName();
-        if(packageName.contains(COM_DOT)){
-            packageName = packageName.substring(4);
-            if(GM.equals(packageName)){
-                packageName = GMAIL;
-            }
+//        if(packageName.contains(COM_DOT)){
+//            packageName = packageName.substring(4);
+//            if(GM.equals(packageName)){
+//                packageName = GMAIL;
+//            }
+//        }
+        if(packageName.contains(GOOGLE_ANDROID_GM)){
+            packageName = GMAIL;
         }
         this.app += packageName;
     }
 
     public String getReminder() {
-        reminder = HEADER + timeToDate() + SPACE + SAYING + SPACE + content + SPACE + app;
+        reminder = HEADER + timeToDate() + SPACE ;
+        if(title != null && !"".equals(title)){
+            reminder += FROM + title;
+        }
+        reminder += SAYING + SPACE + content;
+        if(extraMsg != null && !"".equals(extraMsg)){
+            reminder += SPACE + extraMsg;
+        }
+
         return reminder;
     }
 
