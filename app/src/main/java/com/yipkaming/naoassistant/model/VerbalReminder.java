0a30ef41bca82e26213b808d09014bb429a29735
Rickyip <13002066d@connect.polyu.hk>
2017-02-26 15:59:51 +0800

    - added miss voice call of whatsapp

--- app/src/main/java/com/yipkaming/naoassistant/model/VerbalReminder.java
+++ app/src/main/java/com/yipkaming/naoassistant/model/VerbalReminder.java
@@ -16,13 +16,15 @@ public class VerbalReminder {
     // Greetings
     public static final String CONNECTION_GREETING = "Hi! I am Nao, I have connected to device " + Config.getBluetoothName();
     public static final String INTRODUCTION = "I am your assistant today, " +
-            "You can tell me to read the messages for you by saying read notifications " +
-            "You can also stop this service by saying Stop ASR!";
+            "You can tell me to read the messages by saying read notifications " +
+            "You can also stop this service by saying Stop recognition service!";
     public static final String READ_NOTIFICATION_GREETING = "Do you want me to read the it?";
 
     // Standard message words
     public static final String NO_NOTIFICATION = "You don't have new messages";
     public static final String NEW_NOTIFICATION = "You have new messages, do you want me to read it?";
+
+
     public static final String SAYING = "saying";
     public static final String AGO = "ago";
     public static final String GMAIL = "Gmail";
@@ -46,6 +48,8 @@ public class VerbalReminder {
     public static final String MISSED_CALLS = "missed calls";
     public static final String CALENDER = "com.android.calendar";
     public static final String GMAIL_PACKAGE = "com.google.android.gm";
+    public static final String WHATSAPP = "com.whatsapp";
+    public static final String MISSED_VOICE_CALL = "Missed voice call";
 
 
 
@@ -74,7 +78,7 @@ public class VerbalReminder {
 
     public String getReminder() {
         if(CALLS.equals(this.app)){  // case of missed calls
-            if(tickertext != null && !"".equals(tickertext) && tickertext.contains(MISSED_CALLS)){
+            if(tickertext != null && !"".equals(tickertext.trim()) && tickertext.contains(MISSED_CALLS)){
                 reminder =  "";
                 return reminder;
             }
@@ -89,7 +93,8 @@ public class VerbalReminder {
             return reminder;
         }else if(CALENDER.equals(this.app)){  // case of calender schedule
             return YOU_HAVE_A + SCHEDULE + this.title + AT + this.content;
-        }else if(GMAIL_PACKAGE.equals(this.app)){
+
+        }else if(GMAIL_PACKAGE.equals(this.app)){  // case of email
             int wordCount = 0;
             if(extraMsg != null && !"".equals(extraMsg)){
                 extraMsg = extraMsg.trim();
@@ -105,6 +110,9 @@ public class VerbalReminder {
                 reminder += EMAIL + FROM + title + TITLED + this.extraMsg;
             }
 
+            return reminder;
+        }else if(WHATSAPP.equals(this.app) && MISSED_VOICE_CALL.equals(title)){  // case of email
+            reminder = YOU_HAVE_A + tickertext;
             return reminder;
         }
 
@@ -125,14 +133,10 @@ public class VerbalReminder {
         return greeting+READ_NOTIFICATION_GREETING;
     }
 
-
     private String timeToDate(){
         return DateHelper.getDaysHoursMinutes(Long.valueOf(time));
     }
 }
-
-
-
 
 /*
 Cases design:
@@ -156,5 +160,4 @@ The title is the sender’s email or name, the email title is stored in
 notification content and the email content is stored in extra content.
 Considering the length could be quite long for emails, if the content of
 the email has contained more than 45 words, the verbal reminder will not include the email content.
-
- */
+*/
